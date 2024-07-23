b9a4bb54e7e9b9b4b597ff5e0b654e51f15a98c2
meng <1792766874@qq.com>
2024-07-23 23:48:26 +0800

    feat: 添加get请求发送失败从新发送

--- download/src/lib.rs
+++ download/src/lib.rs
@@ -1,5 +1,7 @@
 use sha1::{Digest, Sha1};
 use std::path::Path;
+use std::thread::sleep;
+use std::time::Duration;
 
 pub mod asset;
 pub mod library;
@@ -9,13 +11,25 @@ pub trait Download {
     fn download(&self, game_dir: &Path) -> Result<(), Box<dyn std::error::Error>>;
 }
 
-pub fn get<T: reqwest::IntoUrl>(url: T) -> reqwest::Result<reqwest::blocking::Response> {
-    let clinet = reqwest::blocking::Client::builder()
+pub fn get<T: reqwest::IntoUrl + Copy>(url: T) -> reqwest::Result<reqwest::blocking::Response> {
+    let client = reqwest::blocking::Client::builder()
         .timeout(None)
         .build()
         .expect("构建客户端失败");
 
-    clinet.get(url).send()
+    let mut retries = 0;
+    let max_retries = 5;
+
+    loop {
+        match client.get(url).send() {
+            Ok(response) => return Ok(response),
+            Err(_e) if retries < max_retries => {
+                retries += 1;
+                sleep(Duration::from_secs(2));
+            }
+            Err(e) => return Err(e),
+        }
+    }
 }
 
 pub fn sha1<P: AsRef<Path>>(path: P) -> Result<String, std::io::Error> {
